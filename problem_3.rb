1667118a06466eb30f6e6eaf6e6087fcc58e49ac
idej <idej.wrk@gmail.com>
2014-04-27 13:56:51 +0300

    clean problem #3 solution

--- problem_3.rb
+++ problem_3.rb
@@ -1,17 +1,14 @@
-num = 600851475143
-nnum = num
-counter = 2
-lagest_fact = 0
+num, counter, lagest_fact = 600851475143, 2, 0
 
-while counter**2 <= nnum do
-  if nnum % counter == 0
-    nnum = nnum / counter
+while counter**2 <= num do
+  if num % counter == 0
+    num = num / counter
     lagest_fact = counter
   else
     counter += 1
   end
 end
 
-lagest_fact = nnum if nnum > lagest_fact
+lagest_fact = num if num > lagest_fact
 
 puts "The largest primefactor of 600851475143 is #{lagest_fact}"
